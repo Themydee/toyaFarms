09ce95cf5937b491b488d9588717adebc2c3327a
Akanbi Bello Temidayo <68824201+Themydee@users.noreply.github.com>
2025-09-20 13:09:43 +0100

    Update App.jsx

--- fd/src/App.jsx
+++ fd/src/App.jsx
@@ -21,7 +21,7 @@ export default function App() {
   const [isProcessingPayment, setIsProcessingPayment] = useState(false);
   const [paymentStatus, setPaymentStatus] = useState(null);
 
-  const PRICE_PER_KG = 4200;
+  const PRICE_PER_KG = 4500;
 
   useEffect(() => {
     fetch("https://toyafarms.onrender.com/api/stock")
